593bfdce5e2c3ff75cd4e493675bbec20819c497
johan.munoz <johansebas14@gmail.com>
2025-04-14 19:25:59 -0500

    Change_folderorigin_20250414

--- python/app.py
+++ python/app.py
@@ -4,6 +4,8 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 
+#FIRST PART --> CREATION OF HEATMAP
+
 # Load the monthly correlation data
 df = pd.read_csv("Monthly_Correlation.csv", index_col=0, parse_dates=True)
 df.index = pd.to_datetime(df.index.astype(str))  # Ensure index is datetime
@@ -37,12 +39,48 @@ def plot_heatmap(data, title):
     )
     return fig
 
+# SECOND PART --> CREATION OF THE CORRELATION BARCHART
+
+# Load the correlation CSV
+df_corr = pd.read_csv("Historical_Correlation.csv", index_col=0, header=None, names=["Stock", "Correlation"],skiprows=1)
+
+#Reset index to have the correct form of the dataframe
+df_corr=df_corr.reset_index()
+
+#turn to float the data
+df_corr["Correlation"] = df_corr["Correlation"].astype(float)
+
+#order the data according to the correlation
+df_corr = df_corr.sort_values("Correlation", ascending=False).reset_index(drop=True)
+
+# Optional: clean column names
+df_corr["Stock"] = df_corr["Stock"].str.replace("_Precio cierre", "", regex=False)
+
+def create_bar_chart():
+    fig = px.bar(
+        df_corr,
+        x="Correlation",
+        y="Stock",
+        orientation="h",
+        title="📊 Historical Correlation with COLCAP since 2020",
+        color="Correlation",
+        color_continuous_scale="RdBu_r",
+        range_color=[-1, 1],
+        labels={"Correlation": "Correlation Coefficient", "Stock": "Stock"}
+    )
+    fig.update_layout(
+        yaxis=dict(autorange="reversed"),
+        height=800,
+        xaxis=dict(tickformat=".2f"))
+    return fig
+
+
 # Dash app setup
 app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
-    html.H1("📊 Monthly Correlation Heatmaps", style={'textAlign': 'center'}),
+    html.H1("📈 Correlation Visualizations", style={'textAlign': 'center'}),
     
     dcc.Dropdown(
         id="group_selector",
@@ -58,7 +96,13 @@ app.layout = html.Div([
     html.Div(
         dcc.Graph(id="heatmap"),
         style={"display": "flex", "justifyContent": "center"}
-    )
+    ),
+    
+    dcc.Tab(label="Historical Correlation Bar Chart", children=[
+            html.Br(),
+            dcc.Graph(figure=create_bar_chart(), style={"margin": "0 auto", "width": "90%"})
+        ])
+    
 ])
 
 @app.callback(
